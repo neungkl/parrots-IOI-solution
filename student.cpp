d748e2bb19fcffb5a521ba4afd4fccd09efda1ae
Kosate Limpongsa <kosatelim@gmail.com>
2016-12-07 00:46:24 +0700

    Ratio 2.62 but very very slow version

--- student.cpp
+++ student.cpp
@@ -1,120 +1,252 @@
 #include "student.h"
 #include <cstdio>
 
-typedef long long llint;
+struct BigInteger {
+  const int LENGTH = 20;
+  const long long NUM_SIZE = 1000000000000000LL;
+
+  long long n[20];
+  int hid;
+
+  BigInteger(int num) {
+    for (int i = LENGTH - 1; i > 0; i--)
+      n[i] = 0;
+    n[0] = num;
+    hid = 1;
+  }
+
+  BigInteger() { BigInteger(0); }
+
+  BigInteger &operator=(int &other) {
+    n[0] = other;
+    hid = 1;
+    return *this;
+  }
+
+  bool operator<=(const BigInteger &other) {
+    if (hid > other.hid)
+      return false;
+    if (hid < other.hid)
+      return true;
+    for (int i = hid - 1; i >= 0; i--) {
+      if (n[i] > other.n[i])
+        return false;
+      if (n[i] < other.n[i])
+        return true;
+    }
+    return true;
+  }
+
+  bool operator<(const BigInteger &other) {
+    if (hid > other.hid)
+      return false;
+    if (hid < other.hid)
+      return true;
+    for (int i = hid - 1; i >= 0; i--) {
+      if (n[i] > other.n[i])
+        return false;
+      if (n[i] < other.n[i])
+        return true;
+    }
+    return false;
+  }
+
+  BigInteger &operator=(const BigInteger &other) {
+    for (int i = 0; i < other.hid; i++) {
+      n[i] = other.n[i];
+    }
+    hid = other.hid;
+    return *this;
+  }
+
+  BigInteger &operator+(const BigInteger &other) {
+    BigInteger *ret = new BigInteger(0);
+
+    long long overflow = 0;
+    int mhid = hid > other.hid ? hid : other.hid;
+    for (int i = 0; i < mhid; i++) {
+      ret->n[i] = overflow;
+      if(i < hid) ret->n[i] = ret->n[i] + n[i];
+      if(i < other.hid) ret->n[i] = ret->n[i] + other.n[i];
+      overflow = ret->n[i] / NUM_SIZE;
+      ret->n[i] %= NUM_SIZE;
+    }
+
+    if (overflow > 0) {
+      ret->n[mhid++] = overflow;
+    }
+    ret->hid = mhid;
+
+    return *ret;
+  }
+
+  BigInteger &operator-(const BigInteger &other) {
+    BigInteger *ret = new BigInteger(0);
+
+    int mhid = hid;
+
+    for (int i = 0; i < mhid; i++) {
+      ret->n[i] += this->n[i] - other.n[i];
+      if (ret->n[i] < 0) {
+        ret->n[i] += NUM_SIZE;
+        ret->n[i + 1]--;
+      }
+    }
+
+    while (ret->n[mhid - 1] == 0 && mhid > 1) {
+      mhid--;
+    }
+
+    ret->hid = mhid;
+
+    return *ret;
+  }
+
+  BigInteger pow(int p) {
+    BigInteger k(n[0]);
+    for (int i = 0; i < p; i++) {
+      k = k + k;
+    }
+    return k;
+  }
+
+  BigInteger shift8() {
+
+    int mod = 1<<8;
+    int left = 0;
+
+    for(int i = hid - 1; i>=0; i--) {
+      n[i] += left * NUM_SIZE;
+      left = n[i] % mod;
+      n[i] /= mod;
+    }
+
+    while (n[hid - 1] == 0 && hid > 1) {
+      hid--;
+    }
+  }
+
+  void print(char *end) {
+    for (int i = hid - 1; i >= 0; i--) {
+      if (i == hid - 1)
+        printf("%lld", n[i]);
+      else
+        printf(" %015lld", n[i]);
+    }
+    printf(":%d", hid);
+    printf(end);
+  }
+};
+
+BigInteger dp[512][256];
 
 void encode(int N, int M[]) {
 
-  const int N_MAX = 16;
-  long long dp[N_MAX * 2][N_MAX];
+  const int N_MAX = 256;
 
   for (int i = 0; i < N_MAX * 2; i++) {
     for (int j = 0; j < N_MAX; j++) {
       if (i == 0) {
         dp[i][j] = j;
       } else if (i == 1 && j == 0) {
-        dp[i][0] = 1 + dp[i - 1][N_MAX - 1];
+        dp[i][0] = dp[i - 1][N_MAX - 1] + 1;
       } else if (j == 0) {
         dp[i][0] = (dp[i - 1][0] - dp[i - 2][0]) + dp[i - 1][N_MAX - 1];
       } else {
-        dp[i][j] = dp[i][j - 1] + (dp[i - 1][j] - dp[i - 1][0]);
+        dp[i][j] = (dp[i - 1][j] - dp[i - 1][0]) + dp[i][j - 1];
       }
     }
   }
 
-  llint num = 0;
-  for (int i = 0, p = 0; i < N; i += 4, p++) {
-    llint n1 = i < N ? M[i] : 0;
-    llint n2 = i + 1 < N ? M[i + 1] : 0;
-    llint n3 = i + 2 < N ? M[i + 2] : 0;
-    llint n4 = i + 3 < N ? M[i + 3] : 0;
-    num = n1 | (n2 << 8) | (n3 << 16) | (n4 << 24);
+  BigInteger num(0);
+  for (int i = 0, p = 0; i < N; i++, p++) {
+    BigInteger num2(M[i]);
+    num = num + num2.pow(i * 8);
+  }
 
-    //printf("==> endcode %lld\n",num);
-
-    int r, c;
-    r = 0, c = 0;
-    while (dp[r][c] <= num) {
-      if (++c >= N_MAX) {
-        c = 0;
-        ++r;
-      }
+  int r, c;
+  r = 0, c = 0;
+  while (dp[r][c] <= num) {
+    if (++c >= N_MAX) {
+      c = 0;
+      ++r;
     }
-    if (--c < 0) {
-      c = N_MAX - 1;
-      --r;
-    }
+  }
+  if (--c < 0) {
+    c = N_MAX - 1;
+    --r;
+  }
 
-    num -= dp[r][c];
+  // num.print("\n");
+  num = num - dp[r][c];
+  // printf("%d %d\n", r, c);
+  send(c);
+  for (--r; r >= 0; --r) {
+    // printf("======#======\n");
+    // num.print("\n");
+    // dp[r][0].print("\n");
+    num = num + dp[r][0];
+    // num.print("\n");
+    while (num < dp[r][c])
+      c--;
+
+    // printf("======\n");
+    // printf("%d %d\n", num < dp[r][c], num < dp[r][c+1]);
+    // num.print("\n");
+    // dp[r][c].print("\n");
+    // dp[r][c+1].print("\n");
+
     // printf("%d\n", c);
-    send((p << 4) | c);
-    for (--r; r >= 0; --r) {
-      num += dp[r][0];
-      while (dp[r][c] > num)
-        c--;
-      // printf("%d\n", c);
-      send((p << 4) | c);
-      num -= dp[r][c];
-    }
+    send(c);
+    num = num - dp[r][c];
+
+    // num.print("\n");
   }
+  // num.print("haha");
 }
 
 //------------------- DO NOT REMOVE NOR EDIT THESE 3 LINES -----------------
 //------------------- above is encode, below is decode ---------------------
 //--------------------cut here----------------------------------------------
 
-#include <vector>
 #include <algorithm>
 using namespace std;
 
 void decode(int N, int L, int X[]) {
 
-  const int N_MAX = 16;
-  long long dp[N_MAX * 2][N_MAX];
+  const int N_MAX = 256;
 
   for (int i = 0; i < N_MAX * 2; i++) {
     for (int j = 0; j < N_MAX; j++) {
       if (i == 0) {
         dp[i][j] = j;
       } else if (i == 1 && j == 0) {
-        dp[i][0] = 1 + dp[i - 1][N_MAX - 1];
+        dp[i][0] = dp[i - 1][N_MAX - 1] + 1;
       } else if (j == 0) {
         dp[i][0] = (dp[i - 1][0] - dp[i - 2][0]) + dp[i - 1][N_MAX - 1];
       } else {
-        dp[i][j] = dp[i][j - 1] + (dp[i - 1][j] - dp[i - 1][0]);
+        dp[i][j] = (dp[i - 1][j] - dp[i - 1][0]) + dp[i][j - 1];
       }
     }
   }
 
-  vector<int> box[16];
+  sort(X, X+L);
+  reverse(X, X+L);
 
-  for (int i = 0; i < L; i++) {
-    box[(X[i] >> 4) & 0xf].push_back(X[i] & 0xf);
+  BigInteger num = 0;
+  int r = L - 1;
+  for (int i = 0; i < L; i++, r--) {
+    if (i == 0) {
+      num = num + dp[r][X[i]];
+    } else {
+      num = num + dp[r][X[i]] - dp[r][0];
+    }
   }
 
-  for (int i = 0, p = 0; i < N; i += 4, p++) {
-    sort(box[p].begin(), box[p].end());
-    reverse(box[p].begin(), box[p].end());
+  // num.print("\n");
 
-    long long num = 0;
-    int r = box[p].size() - 1;
-    for (int i = 0; i < box[p].size(); i++, r--) {
-      if (i == 0) {
-        num += dp[r][box[p][i]];
-      } else {
-        num += dp[r][box[p][i]] - dp[r][0];
-      }
-    }
-
-    //printf("==> decode %lld\n", num);
-
-    if (i < N)
-      output(num & 0xff);
-    if (i + 1 < N)
-      output((num >> 8) & 0xff);
-    if (i + 2 < N)
-      output((num >> 16) & 0xff);
-    if (i + 3 < N)
-      output((num >> 24) & 0xff);
+  for(int i=0; i<N; i++) {
+    output(num.n[0] & 0xff);
+    num.shift8();
   }
 }
